9e5c9f33fd7debab4b60ec85b369f1b56b80cca7
Arman Chowdhury <armanchowdhury449@gmail.com>
2022-03-01 13:33:27 -0500

    second commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
         byte b = (byte) ((byte) (string.charAt(0) - 48) + 10);
         System.out.println(b);
         System.out.println("Hi World !");
+        System.out.println("Hello sir");
     }
 }
