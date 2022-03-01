2355671776baf79718a2657fd6989bd1f64de832
Arman Chowdhury <armanchowdhury449@gmail.com>
2022-03-01 13:31:16 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        String string = "11";
+        byte b = (byte) ((byte) (string.charAt(0) - 48) + 10);
+        System.out.println(b);
+        System.out.println("Hi World !");
+    }
+}
